d099f919e465c5ba4dc9e0712d0206387a45ad90
jbh917 <jbh915@gmail.com>
2019-04-26 15:07:56 +0900

    Initial Commit

---
+++ PreciumWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreciumWallet
+//
+//  Created by 장보현 on 26/04/2019.
+//  Copyright © 2019 장보현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
